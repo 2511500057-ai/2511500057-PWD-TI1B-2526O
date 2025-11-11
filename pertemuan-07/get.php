025cc7d02973fde47f59ee2bb0cf1a86e72f11a9
Steven Marcelino <2511500057@mahasiswa.atmaluhur.ac.id>
2025-11-11 15:23:39 +0700

    session di get_proses.php

--- pertemuan-07/get.php
+++ pertemuan-07/get.php
@@ -1,4 +1,10 @@
-
+<?php
+session_start();
+$_SESSION["nama"] = $_GET["txtNama"];
+$_SESSION["email"] = $_GET["txtEmail"];
+$_SESSION["pesan"] = $_GET["txtPesan"];
+echo "$sesname $sesemail $sespesan";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
