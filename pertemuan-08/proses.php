ad142a3b38057e42474c67193d644c8aeb1a69f3
Chika Zayin Salsabilla <2511500044@mahasiswa.atmaluhur.ac.id>
2025-11-27 10:52:53 +0700

    echo session ke nilai label section contact selesai

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,10 +1,26 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
-$_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
-header("location: index.php");
+
+if (isset($_POST["submit_data"])) {
+  $_SESSION["NIM"] = $_POST["NIM"];
+  $_SESSION["Nama_Lengkap"] = $_POST["Nama_Lengkap"];
+  $_SESSION["Tempat_Lahir"] = $_POST["Tempat_Lahir"];
+  $_SESSION["Tanggal_Lahir"] = $_POST["Tanggal_Lahir"];
+  $_SESSION["Pekerjaan"] = $_POST["Pekerjaan"];
+  $_SESSION["Pasangan"] = $_POST["Pasangan"];
+  $_SESSION["Nama_Ayah"] = $_POST["Nama_Ayah"];
+  $_SESSION["Nama_Ibu"] = $_POST["Nama_Ibu"];
+  $_SESSION["Nama_Kakak"] = $_POST["Nama_Kakak"];
+  $_SESSION["Nama_Adik"] = $_POST["Nama_Adik"];
+}
+
+
+if (isset($_POST["submit_contact"])) {
+  $_SESSION["sesnama"] = $_POST["txtNama"];
+  $_SESSION["sesemail"] = $_POST["txtEmail"];
+  $_SESSION["sespesan"] = $_POST["txtPesan"];
+}
+
+header("Location: index.php");
+exit();
 ?>
--- pertemuan-09/README.md
+++ pertemuan-09/README.md
@@ -1,1 +1,19 @@
-# pertemuan-09
+# pertemuan-09
+
+hari ini, kamis 6 November 2025,
+Nama: Steven Marcelino<br>
+NIM: 2511500057<br>
+<br>
+belajar:<br>
+<ol>
+  <li>Hasil UTS untuk bahan pertemuan-09</li>
+  <li>Membuat form untuk biodata sederhana mahasiswa</li>
+  <li>styling section biodata</li>
+  <li>styling section biodata beres</li>
+  <li>tangkap post dan buat session untuk form biodata</li>
+  <li>session form biodata beres</li>
+  <li>tangkap session dari file proses</li>
+  <li>tangkap session dari file proses selesai</li>
+  <li>echo session ke nilai label section contact</li>
+  <li>echo session ke nilai label section contact selesai</li>
+</ol>
