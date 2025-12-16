32804fd110292d80c5836bb41041d54c4791b691
Steven Marcelino <2511500057@mahasiswa.atmaluhur.ac.id>
2025-12-16 14:08:42 +0700

    Tambahkan kolom nomor urut (bukan ID) pada read.php di kolom pertama

--- pertemuan-11/README.md
+++ pertemuan-11/README.md
@@ -8,4 +8,5 @@ belajar:<br>
 <ol>
 <li>mengcopy file dari pertemuan-10 ke pertemuan-11</li>
 <li>latihan inti sudah selesai</li>
+<li>Tambahkan kolom nomor urut (bukan ID) pada read.php di kolom pertama</li>
 <li>Tambah validasi panjang minimal untuk nama minimal 3 karakter dan pesan minimal 10 karakter</li>
--- pertemuan-11/read.php
+++ pertemuan-11/read.php
@@ -1,24 +1,26 @@
+
 <?php
 require 'koneksi.php';
 
 $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
-$q   = mysqli_query($conn, $sql);
-?>
+$q = mysqli_query($conn, $sql);
 
+$no = 1;
+?>
 <table border="1" cellpadding="8" cellspacing="0">
-<tr>
-    <th>ID</th>
+  <tr>
+    <th>No.</th> <th>ID</th>
     <th>Nama</th>
     <th>Email</th>
     <th>Pesan</th>
-</tr>
+  </tr>
 
-<?php while ($row = mysqli_fetch_assoc($q)): ?>
-<tr>
-    <td><?= $row['cid']; ?></td>
-    <td><?= htmlspecialchars($row['cnama']); ?></td>
-    <td><?= htmlspecialchars($row['cemail']); ?></td>
-    <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-</tr>
-<?php endwhile; ?>
+  <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+      <td><?= $no++; ?></td> <td><?= $row['cid']; ?></td>
+      <td><?= htmlspecialchars($row['cnama']); ?></td>
+      <td><?= htmlspecialchars($row['cemail']); ?></td>
+      <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
+    </tr>
+  <?php endwhile; ?>
 </table>
